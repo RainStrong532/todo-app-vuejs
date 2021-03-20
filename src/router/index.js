6e5825a87151434433f82b0a27b9eb5e5af1b7c7
Mạnh Vũ Văn <vuvanmanhttvtvp@gmail.com>
2021-03-20 11:00:49 +0700

    update routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,11 @@
 import Home from '../components/Home.vue';
 import CardDetail from '../components/CardDetail.vue'
 import ErrorComp from '../components/Error.vue'
+import AddTodo from '../components/AddTodo'
 
 export const routes = [
     { path: '/', component: Home },
-    { path: '/card-detail', component: CardDetail },
-    {path: '*', component: ErrorComp}
+    { path: '/todo/:id', component: CardDetail },
+    {path: '*', component: ErrorComp},
+    {path: '/todos/add', component: AddTodo, name: 'add-todo'},
   ]
